3356f9060b4dbe5ec3e6ed2a95cdab6cdb9a828f
Cyrus <cyhfung@gmail.com>
2024-06-11 18:21:13 -0400

    part 1 start

---
+++ part1/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+    const now = new Date();
+    const a = 10;
+    const b = 20;
+    console.log(now, a+b)
+
+    return (
+        <div>
+            <p>Hello world, it is {now.toString()}</p>
+        </div>
+    )
+}
+
+export default App
